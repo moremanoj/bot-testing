aac502c1feddc408be45fcbc26c5fc044a405535
manoj <manoj@hasura.io>
2017-02-21 10:33:25 +0530

    simple airline test

--- bot-testing/api/config.js
+++
@@ -1,8 +0,0 @@
-if (document.location.hostname == "80Shops.com" || document.location.hostname == "www.80Shops.com"){
-	var baseURL = "https://www.80Shops.com/api/";
-	var imageURL = "https://www.80Shops.com/assets/";
-} else {
-	var baseURL= "http://localhost:7000/api/";
-	//var baseURL = "https://www.createmobileshop.com/api/";
-	var imageURL = "https://www.80Shops.com/assets/";
-}
--- bot-testing/webserver.js
+++ bot-testing/webserver.js
@@ -1,38 +1,22 @@
 require('use-strict');
 async = require("async");
 var connect = require('connect');
-var app = connect();
 var path = require('path');
-
 var bodyParser = require('body-parser');
 var serveStatic = require('serve-static');
 var vhost = require('vhost');
 
-var serverport = 7000;
+var serverport = 7575;
 
+var app = connect();
 // Define the web folder connect and router
 var web = connect();
-var html = connect();
-var superadmin = connect();
-var admin = connect();
-var assets = connect();
 
-var app = require('../api/api');
+//var app = require('../api/api');
 
 web.use(serveStatic('public'));
 app.use('/',web);
 
-html.use(serveStatic('html'));
-app.use('/html',html);
-
-superadmin.use(serveStatic('superadmin'));
-app.use('/superadmin',superadmin);
-
-admin.use(serveStatic('admin'));
-app.use('/admin',admin);
-
-assets.use(serveStatic('../assets'));
-app.use('/assets',assets);
 
 //console.log(__dirname);
 global.appRoot = path.resolve(__dirname);
