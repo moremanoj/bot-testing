36671f71073df98f0fe07a5e0b961b1b7c158377
manoj <manoj@hasura.io>
2017-02-21 10:51:27 +0530

    new file botserver.js

---
+++ bot-testing/botserver.js
@@ -0,0 +1,25 @@
+require('use-strict');
+async = require("async");
+var connect = require('connect');
+var path = require('path');
+var bodyParser = require('body-parser');
+var serveStatic = require('serve-static');
+var vhost = require('vhost');
+
+var serverport = 7575;
+
+var app = connect();
+// Define the web folder connect and router
+var web = connect();
+
+//var app = require('../api/api');
+
+web.use(serveStatic('public'));
+app.use('/',web);
+
+
+//console.log(__dirname);
+global.appRoot = path.resolve(__dirname);
+app.listen(serverport, function () {
+  console.log('listening on port '+serverport+'!');
+});
